777bfff7bf09a9e14fa954548a34ff041736c973
Sergey Khursevich <sergey.khursevich@sast-mfa.com>
2024-05-08 13:01:47 +0200

    feat: add creating user

--- actions/register.ts
+++ actions/register.ts
@@ -2,12 +2,25 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schema";
+import { getUserByEmail, saveUserData } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
 
     if (!validateFields.success) {
         return { error: "Invalid fields" };
+    }
+
+    const user = await getUserByEmail(values);
+
+    if (user) {
+        return { error: "User already exists" };
+    }
+
+    const userData = await saveUserData(values);   
+    
+    if (!userData) {
+        return { error: "Error creating user" };
     }
 
     return { success: "Creating an account has been successful" };
